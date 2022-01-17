8a92ec87dcb907c89a5caa9989b19201907d319c
shirleyxx <74038718+surelyshirleyxx@users.noreply.github.com>
2022-01-17 16:32:03 -0700

    Update countingvowels1.py
    counting constants too :)

--- countingvowels1.py
+++ countingvowels1.py
@@ -1,4 +1,5 @@
-nname = input("Today you'll get to learn how many vowels are in the a sentence/word you input. Type your word/sentence here: ")
+name = input("Today you'll get to learn how many vowels are in the a sentence/word you input. Type your word/sentence here: ")
 name = name.lower()
-count = len([i for i in name if i in 'aeiou'])
-print('Number of vowels in your word/sentence: ' + str(count))
+count_vowel = len([i for i in name if i in 'aeiou'])
+count_constant = len([i for i in name if i in 'bcdfghjklmnpqrstvwxyz'])
+print('Number of vowels in your word/sentence: ' + str(count_vowel) + '.' + ' The number of constants in your word/sentence: ' +str(count_constant))
