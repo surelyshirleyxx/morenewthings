f3a25dff94d25134c65cbcdf2e94e868eca6b4a9
shirleyxx <74038718+surelyshirleyxx@users.noreply.github.com>
2022-01-17 14:50:51 -0700

    Add files via upload

---
+++ countingvowels.py
@@ -0,0 +1,3 @@
+name = input("Today you'll get to learn how many vowels are in the a sentence/word you input. Type your word/sentence here: ")
+count = len([i for i in name if i in 'aeiou'])
+print('Number of vowels in your word/sentence: ' + str(count))
