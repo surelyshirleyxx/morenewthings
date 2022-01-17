6bd349de6301b73c0e5b5bd0874abd725af253d6
shirleyxx <74038718+surelyshirleyxx@users.noreply.github.com>
2022-01-17 16:03:30 -0700

    Update countingvowels1.py

--- countingvowels1.py
+++ countingvowels1.py
@@ -1,3 +1,4 @@
-name = input("Today you'll get to learn how many vowels are in the a sentence/word you input. Type your word/sentence here: ")
-count = len([i for i in name if i in 'aeiouAEIOU'])
+nname = input("Today you'll get to learn how many vowels are in the a sentence/word you input. Type your word/sentence here: ")
+name = name.lower()
+count = len([i for i in name if i in 'aeiou'])
 print('Number of vowels in your word/sentence: ' + str(count))
