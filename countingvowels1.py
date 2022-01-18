5ca04320b5833eb79bb90c7e12bf69519415235e
shirleyxx <74038718+surelyshirleyxx@users.noreply.github.com>
2022-01-17 17:04:25 -0700

    Update countingvowels1.py

--- countingvowels1.py
+++ countingvowels1.py
@@ -1,5 +1,5 @@
-name = input("Today you'll get to learn how many vowels are in the a sentence/word you input. Type your word/sentence here: ")
+name = input("Today you'll get to learn how many vowels and consonants are in the sentence/word you input. Type your word/sentence here: ")
 name = name.lower()
 count_vowel = len([i for i in name if i in 'aeiou'])
-count_constant = len([i for i in name if i in 'bcdfghjklmnpqrstvwxyz'])
-print('Number of vowels in your word/sentence: ' + str(count_vowel) + '.' + ' The number of constants in your word/sentence: ' +str(count_constant))
+count_consonant = len([i for i in name if i in 'bcdfghjklmnpqrstvwxyz'])
+print('Number of vowels in your word/sentence: ' + str(count_vowel) + '.' + ' The number of consonants in your word/sentence: ' +str(count_consonant))
